7d754c49ed57bcfdc73517a38ecb8a29f28e5c86
jjmckenzie <>
2018-06-26 13:41:15 -0400

    updated schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,6 @@
-ALTER TABLE tbl AUTO_INCREMENT = 100001;
+ALTER TABLE users AUTO_INCREMENT = 100001;
+ALTER TABLE users CHANGE COLUMN `createdAt` `createdAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE users CHANGE COLUMN `updatedAt` `updatedAt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP;
 INSERT INTO users (firstName, lastName, email, skill) 
 VALUES ('Ryan', 'Diaz', 'ryandiaz@gmail.com', 'JavaScript'), ('Jimmy', 'McKenzie', 'jimmy.j.mckenzie@gmail.com', 'HTML');
 SELECT * FROM main.users;
