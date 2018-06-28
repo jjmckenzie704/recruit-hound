764e0041cdb18939113fc98df5859ebd84e7dd48
Ryan Diaz <ryandiaz@gmail.com>
2018-06-28 18:18:42 -0400

    Started login script

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -16,9 +16,6 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  app.get("/login", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
 
 
 };
--- server.js
+++ server.js
@@ -2,31 +2,42 @@
 // Server.js - This file is the initial starting point for the Node/Express server.
 //
 // ******************************************************************************
-// *** Dependencies
+
+// Dependencies
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var Bcrypt = require('bcrypt'); // use bcrypt to hash passwords.
-var Joi = require('joi');
-var custom_fields = {
-  email     : Joi.string().email().required(), // Required
-  password  : Joi.string().required().min(6)   // minimum length 6 characters
-}
+var session  = require('express-session');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
+require('./config/passport')(passport); // pass passport for configuration
 
 // Requiring our models for syncing
 var db = require("./models");
 
 // Sets up the Express app to handle data parsing
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.urlencoded({ extended: true }));  // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());  // parse application/json
+app.set('view engine', 'ejs'); // set up ejs for templating
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
+// Required for passport
+app.use(session({
+	secret: 'ryanjimmychadmatt',
+	resave: true,
+	saveUninitialized: true
+ } )); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
 
 // Static directory
 app.use(express.static("public"));
@@ -36,6 +47,7 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 require("./routes/recruiter-routes.js")(app);
+require('./routes/passport-routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
