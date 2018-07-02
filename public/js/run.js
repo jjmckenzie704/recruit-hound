f7445fee7ff2359263107086b3522d461328fd16
Matt <matthewgeddes@yahoo.com>
2018-07-01 21:58:17 -0400

    redoing ui

--- public/js/run.js
+++ public/js/run.js
@@ -1,37 +1,4 @@
 
-let $loginMsg = $('.loginMsg'),
-    $login = $('.login'),
-    $signupMsg = $('.signupMsg'),
-    $signup = $('.signup'),
-    $frontbox = $('.frontbox');
-  
-  $('#switch1').on('click', function() {
-    $loginMsg.toggleClass("visibility");
-    $frontbox.addClass("moving");
-    $signupMsg.toggleClass("visibility");
-  
-    $signup.toggleClass('hide');
-    $login.toggleClass('hide');
-  })
-  
-  $('#switch2').on('click', function() {
-    $loginMsg.toggleClass("visibility");
-    $frontbox.removeClass("moving");
-    $signupMsg.toggleClass("visibility");
-  
-    $signup.toggleClass('hide');
-    $login.toggleClass('hide');
-  })
-  
-  setTimeout(function(){
-    $('#switch1').click()
-  },1000)
-  
-  setTimeout(function(){
-    $('#switch2').click()
-  },3000)
-
-
 let $j = jQuery.noConflict();
 
 $j( document ).ready(function() {
